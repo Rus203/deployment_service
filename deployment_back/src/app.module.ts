03f0ce6a5349d138edcaf70dd46edbb81d3dda44
Ruslan <c8man@gmail.com>
2023-04-14 01:21:24 +0300

    feat: add a getting statistic endpoint

--- deployment_back/src/app.module.ts
+++ deployment_back/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServerModule } from './server/server.module';
 import typeOrmConfig from './configs/type-orm.config';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [UserModule, TypeOrmModule.forRoot(typeOrmConfig), ServerModule],
   controllers: [],
   providers: [],
 })
---
+++ deployment_back/src/server/server.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import * as os from 'os';
+
+@Injectable()
+export class ServerService {
+  getStatus() {
+    const totalMemory = os.totalmem();
+    const freeMemory = os.freemem();
+    const loadAverage = os.loadavg();
+    return { totalMemory, freeMemory, loadAverage };
+  }
+}
