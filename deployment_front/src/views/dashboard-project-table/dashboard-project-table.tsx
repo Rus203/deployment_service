bb5d7b87dee50f4e17656b1c0baef50e666ab04b
Ruslan <c8man@gmail.com>
2023-07-05 12:32:11 +0200

    feat: add health checker

--- deployment_front/src/views/dashboard-project-table/dashboard-project-table.tsx
+++ deployment_front/src/views/dashboard-project-table/dashboard-project-table.tsx
@@ -15,6 +15,7 @@ import axios from 'axios'
 import { setProjectCollection } from '../../store/Slices/project.slice'
 import { IProject } from '../../interface/project.interface'
 import Spinner from '../../Components/Spinner'
+import HealthCheck from '../../Components/Heath-Check'
 
 const DashBoardProjectTable: FC = () => {
   const [loading, setLoading] = useState<boolean>(true)
@@ -24,6 +25,7 @@ const DashBoardProjectTable: FC = () => {
     .find(item => item.id === miniBackId)
   const dispatch = useAppDispatch()
   const projects = useAppSelector(state => state.project.projectCollection)
+  const host = useAppSelector(state => state.health.host)
 
   useLayoutEffect(() => {
     if (miniBack) {
@@ -41,46 +43,49 @@ const DashBoardProjectTable: FC = () => {
   }, [])
 
   return miniBack ? (
-    <Container>
-      <Card>
-        <TableContainer>
-          <FixedTable>
-            { loading ? <SpinBlock><Spinner typeOfMessages={null} /></SpinBlock> : (
-              <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
-              <TableHead>
-                <TableRow>
-                  <TableCell>№</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Git link</TableCell>
-                  <TableCell>Status</TableCell>
-                  <TableCell colSpan={4} align='center'>Controls</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {(projects !== undefined && projects.map((project: IProject, index: number) => (
-                  <TableItemProject
-                    key={project.id}
-                    index={index + 1}
-                    project={project}
-                    serverUrl={miniBack.serverUrl}
-                    port={miniBack.port}
-                  />
-                )))}
-              </TableBody>
-            </Table>
-            )}
-          </FixedTable>
-        </TableContainer>
-      </Card>
-      <ControlButtons>
-        <LinkToDeploy href='/'>
-          <Button variant="contained">Back</Button>
-        </LinkToDeploy>
-        <LinkToDeploy href='project'>
-          <Button variant="contained">Create</Button>
-        </LinkToDeploy>
-      </ControlButtons>
-    </Container>
+    <>
+      <Container>
+        <Card>
+          <TableContainer>
+            <FixedTable>
+              { loading ? <SpinBlock><Spinner /></SpinBlock> : (
+                <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>№</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Git link</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell colSpan={4} align='center'>Controls</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {(projects !== undefined && projects.map((project: IProject, index: number) => (
+                    <TableItemProject
+                      key={project.id}
+                      index={index + 1}
+                      project={project}
+                      serverUrl={miniBack.serverUrl}
+                      port={miniBack.port}
+                    />
+                  )))}
+                </TableBody>
+              </Table>
+              )}
+            </FixedTable>
+          </TableContainer>
+        </Card>
+        <ControlButtons>
+          <LinkToDeploy href='/'>
+            <Button variant="contained">Back</Button>
+          </LinkToDeploy>
+          <LinkToDeploy href='project'>
+            <Button variant="contained">Create</Button>
+          </LinkToDeploy>
+        </ControlButtons>
+      </Container>
+      { host ? <HealthCheck host={host} /> : null }
+    </>
   ) : null
 }
 
